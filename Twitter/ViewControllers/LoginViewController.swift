29cd2a4933d6d0b5399a57a4e1021d5a243c9e22
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-01-24 01:31:49 -0800

    Commit Step 4 Remember Login/out

--- Twitter/ViewControllers/LoginViewController.swift
+++ Twitter/ViewControllers/LoginViewController.swift
@@ -1,11 +1,14 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
     @IBAction func loginButton(_ sender: Any) {
         
         let myURL = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: myURL, success: {
+            // When login successfully
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not login")
@@ -18,6 +21,12 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         loginButton.layer.cornerRadius = 10
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
     }
 
 
