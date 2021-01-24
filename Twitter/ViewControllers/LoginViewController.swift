ff3ddb4fe21ef4e78829fc27f3f667054f2660f8
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-01-23 18:51:49 -0800

    Commit Step 3 logout

--- Twitter/ViewControllers/LoginViewController.swift
+++ Twitter/ViewControllers/LoginViewController.swift
@@ -1,13 +1,6 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
     @IBAction func loginButton(_ sender: Any) {
         
         let myURL = "https://api.twitter.com/oauth/request_token"
@@ -19,14 +12,13 @@ class LoginViewController: UIViewController {
         })
     }
     
-    /*
-    // MARK: - Navigation
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        loginButton.layer.cornerRadius = 10
     }
-    */
+
 
 }
